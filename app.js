d933727f52dbd7d420c9e979ceb1c3693e0bb16a
Martin Gruber <mortlp121@gmail.com>
2025-04-20 14:47:59 +0200

    Static serving,CORS, Docker

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const imageRoutes = require('./routes/image')
 const rateLimit = require('express-rate-limit');
-
-
+const path = require('path');
+const cors = require('cors');
 const globalLimiter = rateLimit({
     windowMs: 10 * 60 * 1000,
     max: 100, 
   });
 dotenv.config();
-
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', imageRoutes);
